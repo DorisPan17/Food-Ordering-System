4372e10668fc5338342c24a4198d7ef97cb7f0ab
Doris Pan <122941061+DorisPan17@users.noreply.github.com>
2023-09-05 16:16:09 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from LoginPage import LoginPage
+
+
+class NuCafe:
+    def __init__(self):
+        root = tk.Tk()
+        root.geometry("800x500")
+        login = LoginPage(root) # creat a object of class 'login_page'
+        login.setupWindow() 
+        login.run()
+
+        root.mainloop()
+
+
+nuCafeMain = NuCafe()
+
